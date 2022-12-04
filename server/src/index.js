b1db976826b22c05aa89cbf3cf4e771bfb28fe82
aremartins <arealbmartinsan@gmail.com>
2022-12-04 14:46:25 -0300

    fix: corrige servidor

--- server/src/index.js
+++ server/src/index.js
@@ -4,24 +4,26 @@ const bodyParser = require('body-parser');
 const multipart = require('connect-multiparty');
 
 const app = express();
-app.arguments(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const cors = {
+const corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 const multipartMiddleware = multipart({ uploadDir: './uploads' });
-app.post('/upload', multipartMiddleware, (req, res)=> {
+app.post('/upload', multipartMiddleware, (req, res) => {
   const files = req.files;
-  console.log(files)
-  req.json({ message: files })
-} )
+  console.log(files);
+  res.json({ message: files });
+});
 
-app.use((err, req, res, next) => req.json({error: err.message}))
+
+
+app.use((err, req, res, next) => res.json({error: err.message}));
 
 app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+  console.log('Servidor porta 8000');
 })
